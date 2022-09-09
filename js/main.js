20955b28f1109afb6157addc4244267ceef7e0cd
akramjonnuriddinov <akramjonmohirdev@gmail.com>
2022-09-10 01:35:04 +0500

    Add scrollbar, country-list

--- js/main.js
+++ js/main.js
@@ -4,6 +4,19 @@ const elToggleIconUp = document.querySelector('.toggle-icon-up');
 const elContainer = document.querySelector('.container');
 const elCountryWrapper = document.querySelector('.country-wrapper');
 
+// COUNTRY-LIST
+const elsFormCountryItem = document.querySelectorAll('.form__country-item');
+const elsFormCountryName = document.querySelectorAll('.form__country-name');
+
+if(elsFormCountryItem) {
+  elsFormCountryItem.forEach(function(item, index) {
+    item.addEventListener('click', function() {
+      elFormCountry.value = elsFormCountryName[index].outerText;
+      elCountryWrapper.classList.remove('country-wrapper--open');
+    });
+  })
+}
+
 // COUNTRY
 if(elFormCountry) {
   elFormCountry.addEventListener('focus', function() {
@@ -12,3 +25,4 @@ if(elFormCountry) {
     elCountryWrapper.classList.add('country-wrapper--open');
   });
 }
+
